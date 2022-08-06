7331be55b543af163868840e3c2d873f18cbfa3c
Leandro Facim <leandro.facim@hotmail.com>
2022-08-06 17:01:49 -0300

    feat: adds user repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,31 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    return this.findOne({
+      where: {
+        name,
+      },
+    });
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return this.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}
+
+export default UsersRepository;
